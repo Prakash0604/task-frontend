f58fd09fc5a5e0bd15ac69c4d981b6c423de7423
imkrprashant18 <prashantmahato77@gmail.com>
2025-04-10 09:40:59 +0545

    ui install

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 "use client"
 
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <>
       <h1>Hello bun js</h1>
+      <Button variant='outline'>Hello</Button>
     </>
   );
 }
